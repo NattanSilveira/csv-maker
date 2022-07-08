4fae50e467875af0c245e5d017b5c7009d142010
Nattan Sousa <nattan_lion@msn.com>
2022-07-08 13:40:07 -0300

    feat: Criando rota para o envio do json

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['validJson']], function () {
+    Route::group(['prefix' => 'csv-queue'], function (){
+        Route::post('/', [\App\Http\Controllers\Api\CsvMakerController::class, 'store']);
+    });
 });
