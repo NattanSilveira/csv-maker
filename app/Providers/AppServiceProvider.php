88dd8418efc72cb8a555321bc0dd2c43f43385be
Nattan Sousa <nattan_lion@msn.com>
2022-07-11 14:49:49 -0300

    feat: Adicionando logica para a execução do export após terminar todos os jobs da fila

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,16 @@
 
 namespace App\Providers;
 
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
+use ReflectionProperty;
 
 class AppServiceProvider extends ServiceProvider
 {
+    private $queue = [];
     /**
      * Register any application services.
      *
@@ -23,6 +29,44 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Queue::after(function (JobProcessed $event) {
+
+            $payload = json_decode( $event->job->getRawBody() );
+            $data = unserialize($payload->data->command);
+            $property = new ReflectionProperty($data, 'request');
+            $property->setAccessible(true);
+
+            $csvQueue = DB::table('jobs')->whereQueue('csv-queue')->get();
+
+            array_push($this->queue, (array)$property->getValue($data));
+
+            if ($csvQueue->count() <= 0) {
+
+                try {
+                    $filename = "json-to-csv.csv";
+                    $path = storage_path('app/public/');
+
+                    $handle = fopen($path.$filename, 'w');
+
+                    $this->queue = array_reverse($this->queue);
+
+                    foreach ($this->queue as $key => $item) {
+                        $header = (array_keys($this->queue[$key]));
+                        fputcsv($handle,  $header, ';');
+                        fputcsv($handle,  $item);
+
+                    }
+
+                    fclose($handle);
+                } catch(\Exception $ex){
+                    Log::info($ex);
+                }
+
+
+            }
+
+        });
+
     }
+
 }
