a14e8de0056ae808254c1609387645f93219d232
Nattan Sousa <nattan_lion@msn.com>
2022-07-11 14:49:08 -0300

    feat: Criando job para a criação do csv

---
+++ app/Jobs/CsvMake.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+
+class CsvMake implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $request;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = json_decode($request->getContent());
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+    }
+}
